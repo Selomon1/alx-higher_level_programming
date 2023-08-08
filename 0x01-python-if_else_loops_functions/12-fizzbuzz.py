c46f3ce04ae93e5aafdef8afd092d875c1cb769c
Selomon1 <soya.vandy1950@gmail.com>
2023-08-08 13:41:12 -0700

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for mul in range(1, 101):
-        if (mul % 3 == 0 and  mul % 5 == 0):
+        if (mul % 3 == 0 and mul % 5 == 0):
             print("FizzBuzz ", end="")
         elif (mul % 3 == 0):
             print("Fizz ", end="")
         elif (mul % 5 == 0):
-            print("Buzz ", end ="")
+            print("Buzz ", end="")
         else:
             print("{} ".format(mul), end="")
