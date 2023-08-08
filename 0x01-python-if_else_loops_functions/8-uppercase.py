52ac267cda6cc8b335f80304d7142662bbdbf836
Selomon1 <soya.vandy1950@gmail.com>
2023-08-08 12:48:18 -0700

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        lett = ord(str[c])
+    for u in range(len(str)):
+        lett = ord(str[u])
         if lett >= 97 and lett <= 122:
             lett = lett - 32
         print('{:c}'.format(lett), end='')
