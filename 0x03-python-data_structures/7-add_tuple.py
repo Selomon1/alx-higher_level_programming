6e11cf9d92a0fa48b88e89641d547ae87a539d6b
Selomon1 <soya.vandy1950@gmail.com>
2023-08-14 11:31:17 -0700

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,5 +8,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(Ele_b) < 2:
         for i in range(len(Ele_b), 2):
             Ele_b.append(0)
-    add_tuple = [Ele_a[0] + Ele_b[0] + Ele_a[1] + Ele_b[1]]
+    add_tuple = (Ele_a[0] + Ele_b[0], Ele_a[1] + Ele_b[1])
     return tuple(add_tuple)
