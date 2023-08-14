5133e8936bc1bcbe43fb683d9779f3546057b15d
Selomon1 <soya.vandy1950@gmail.com>
2023-08-14 08:14:46 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string != [c, C]:
+    if my_string[i] != [c, C]:
         return my_string
     else:
         print(my_string.translate({ord(i): None for i in 'cC'}))
