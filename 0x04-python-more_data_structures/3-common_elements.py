e1cdb1deaa24a95e35f2dbd77bafb49f591b84da
Selomon1 <soya.vandy1950@gmail.com>
2023-08-16 03:57:50 -0700

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    list1 = set(set_1)
-    list2 = set(set_2)
-    if (list1 == list2):
-        return (list1 & list2)
+    if x in set_1 == x in set_2:
+        return x
