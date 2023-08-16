b8761163ba2b7edd51e429d2b30e95029b87979e
Selomon1 <soya.vandy1950@gmail.com>
2023-08-16 15:47:02 -0700

    P_More_Data

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,8 +13,8 @@ def roman_to_int(roman_string):
     }
     sum = 0
     pre = 0
-    for i in range(len(roman_string) - 1, -1, -1):
-        if nu[roman_string[i] >= pre:
+    for i in range(len(roman_string)):
+        if nu[roman_string[i]] >= pre:
             sum += nu[roman_string[i]]
         else:
             sum -= nu[roman_string[i]]
