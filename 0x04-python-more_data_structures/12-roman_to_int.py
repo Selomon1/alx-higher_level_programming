ea05a9b41b86974d926671b404255d31fa911c2d
Selomon1 <soya.vandy1950@gmail.com>
2023-08-16 14:32:15 -0700

    P_More_Data

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    if type(roman_string) is not str and roman_string is None:
         return 0
 
     nu = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
