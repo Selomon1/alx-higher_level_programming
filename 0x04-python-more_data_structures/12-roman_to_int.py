e1bbd984ec7df785940f4cb64fb3cd4f5f917cdb
Selomon1 <soya.vandy1950@gmail.com>
2023-08-16 15:21:59 -0700

    P_More_Data

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,21 +3,20 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return None
     nu = {
-        'M': 1000,
-        'D': 500,
-        'C': 100,
-        'L': 50,
+        'I': 1,
+        'V': 50,
         'X': 10,
-        'V': 5,
-        'I': 1
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
     sum = 0
     pre = 0
     for i in range(len(roman_string) - 1, -1, -1):
-        value = nu[roman_string[i]]
-        if value >= pre:
-            sum += value
+        if nu[roman_string[i] >= pre:
+            sum += nu[roman_string[i]]
         else:
-            sum -= value
-        pre = value
+            sum -= nu[roman_string[i]]
+        pre = nu[roman_string[i]]
     return sum
