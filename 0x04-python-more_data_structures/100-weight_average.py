c7fd49ab1d6b6887e861738c97dbd4a1d6863f39
Selomon1 <soya.vandy1950@gmail.com>
2023-08-17 03:48:38 -0700

    P_More_data

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+i#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_lists == NULL:
+        return 0
+    po = 0
+    for k, weight in my_list:
+        po += k * weight
+        average = po / sum(weight)
+    return average
