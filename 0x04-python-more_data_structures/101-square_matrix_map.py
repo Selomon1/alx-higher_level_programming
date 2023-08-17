15b47ee36201494c8ae51dc7d61f524381e7751e
Selomon1 <soya.vandy1950@gmail.com>
2023-08-17 05:31:02 -0700

    P_More_data

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    m = list(map(lambda i: list(map(lambda x: pow(x, 2), i)), matrix))
-    return m
+    return list(map(lambda i: list(map(lambda x: pow(x, 2), i)), matrix))
