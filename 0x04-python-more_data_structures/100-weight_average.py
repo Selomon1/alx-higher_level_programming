3002548b81f634c2605df5f564c0c3a4e751ec73
Selomon1 <soya.vandy1950@gmail.com>
2023-08-17 04:55:32 -0700

    P_More_data

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list is None:
         return 0
     po = 0
-    nat = 0
     for k, weight in my_list:
         po += k * weight
-        nat += weight
-    return nat
+        average = po / sum(weight)
+    return average
