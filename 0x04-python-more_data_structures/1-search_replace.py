bf3ade0be856ee60142cc6b6c05f1723fb80667e
Selomon1 <soya.vandy1950@gmail.com>
2023-08-17 02:53:46 -0700

    More_data

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list,search, replace):
     new_lists = [:]
-    for i in range(len(new_lists)):
+    for i in new_lists:
         if new_lists[i] == search:
             new_lists[i] = replace
     return new_lists
