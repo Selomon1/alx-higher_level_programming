c1b328ea365ef7f52d87948b6a1524ef8a633f36
Selomon1 <soya.vandy1950@gmail.com>
2023-08-17 02:47:40 -0700

    More_data

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list,search, replace):
-    new_lists = []
-    return (list(map(lambda x: x.replace(new_lists, my_list))))
+    new_lists = [:]
+    return (list(map(lambda x: x.replace(my_list, new_lists), new_lists)))
