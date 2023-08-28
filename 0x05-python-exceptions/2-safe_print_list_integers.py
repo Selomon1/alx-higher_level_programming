43bff6cd11237065482ca3e1e8981c79043fac13
Selomon1 <soya.vandy1950@gmail.com>
2023-08-28 11:01:58 -0700

    Python Exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-
+    rel_no = 0
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end='')
+            rel_no = rel_no + 1
+        except (ValueError, TypeError):
+            continue
+    print('')
+    return rel_no
