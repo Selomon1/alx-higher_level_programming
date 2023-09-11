3731d9e9ee49db9aca72fdbda5ee3f264d609b35
Selomon1 <soya.vandy1950@gmail.com>
2023-09-11 14:08:23 -0700

    Python Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,5 +15,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """represent the set of string in the rectangle"""
-        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
-                                        self.__size, self.__size)
+        return "[{}] {}/{}".format(type(self).__name, self.__size, self.__size)
