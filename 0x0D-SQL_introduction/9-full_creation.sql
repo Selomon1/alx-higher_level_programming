cccf9bd354c494fbfbd3a27757609e69695afe96
Selomon1 <soya.vandy1950@gmail.com>
2023-10-18 11:49:59 -0700

    SQL Introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- Script that creates a table secon_table in the database
 -- and add multiples rows
 CREATE TABLE IF NOT EXISTS second_table (id INT,
-name VARCHAR(256), score INT);
+name VARCHAR(256),
+score INT);
 
 INSERT INTO second_table(id, name, score) VALUES(1, 'Jhon', 10);
 INSERT INTO second_table(id, name, score) VALUES(2, 'Alex', 3);
