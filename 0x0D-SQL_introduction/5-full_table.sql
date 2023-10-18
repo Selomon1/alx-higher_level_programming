6d9d0371949a5e2fbdadb37862bdc979c48d3d03
Selomon1 <soya.vandy1950@gmail.com>
2023-10-18 06:25:46 -0700

    SQL Introduction

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Script that prints the full description of the table
+-- from the datbase hbtn_0c_0
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Scripts that lists all allows of the table first_table
+-- from the current database
+SELECT * FROM first_table;
