4a9b933fe6cff31564612aef62fa890feafc5ec0
Selomon1 <soya.vandy1950@gmail.com>
2023-11-13 10:53:10 -0800

    JavaScript Warm Up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,8 @@
 const numbers = process.argv.slice(2);
 
 if (numbers.length > 1) {
-  numbers.sort();
-  SecBig = numbers[numbers.length - 2];
+  numbers.sort(function (x, y) { returrn (x - y); });
+  console.log(numbers[1]);
+} else {
+  console.log(0);
 }
-console.log(0);
