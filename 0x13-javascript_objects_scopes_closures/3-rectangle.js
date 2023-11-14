03cf56bda7734448989891a53608fc3ea6de58d7
Selomon1 <soya.vandy1950@gmail.com>
2023-11-14 10:01:35 -0800

    JavaScript Object Scope

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-		  return;
-	  }
-	  this.width = w;
-	  this.height = h;
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
   }
 
   print () {
-	  let loop = 0;
-	  while (loop < this.height) {
-		  console.log('X'.repeat(this.width));
-		  loop++;
-	  }
+    let loop = 0;
+    while (loop < this.height) {
+      console.log('X'.repeat(this.width));
+      loop++;
+    }
   }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    for (let loop = 0; loop < this.height; loop++) {
+      if (c === undefined) {
+        c = 'X';
+      }
+      console.log(c.repeat(this.width));
+    }
+  }
+};
