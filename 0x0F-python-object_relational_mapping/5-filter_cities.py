a6275cdb6dfafd2180e6011b8146e58899c67fc3
Selomon1 <soya.vandy1950@gmail.com>
2023-11-19 14:26:06 -0800

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,7 +4,7 @@ Script that takes in the name of a state as an argument and lists all cities
 of the state using the data base hbtn_0e_4_usa
 """
 
-
+i
 import sys
 from sys import argv
 import MySQLdb
@@ -13,13 +13,14 @@ if __name__ == "__main__":
     da = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset='utf8')
     con = da.cursor()
-    sql = """
-    SELECT cities.name FROM cities INNER JOIN states
-    ON cities.states_id=state.id WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
+    sql = "SELECT cities.name FROM cities WHERE state_id =\
+          (SELECT id FROM states WHERE name = %s) ORDER BY cities.id ASC"
     con.execute(sql, (argv[4],))
     rows = con.fetchall()
-    print(", ".join(c[0] for c in rows)
+    for c in rows:
+        print(c[0], end='')
+        if (c != rows[-1]):
+            print(end=', ')
+    print()
     con.close()
     da.close()
