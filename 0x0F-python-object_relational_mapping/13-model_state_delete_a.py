492bbbefd620285675750a9a5267dfe086380a5e
Selomon1 <soya.vandy1950@gmail.com>
2023-11-19 12:17:23 -0800

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,7 +15,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine("mysql://{argv[1]}:{argv[2]}@localhost:\
             3306/{argv[3]}")
-    Base.metadata.create_all()
+    Base.metadata.create_all(engine)
     Sesion = session maker(bind=engine)
     session = Session()
     states = session.query(State).filter(State.name.like('%a%'))
