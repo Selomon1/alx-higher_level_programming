ef2d64c0d0f21363fd0bb8de498f4ccc008844b9
Selomon1 <soya.vandy1950@gmail.com>
2023-11-19 14:01:05 -0800

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,15 +5,16 @@ of the state using the data base hbtn_0e_4_usa
 """
 
 
-import MySQLdb
 import sys
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    da = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    da = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     con = da.cursor()
     con.execute("SELECT * FROM cities JOIN states \
                 ON cities.states_id = state.id ORDER BY cities.id")
-    [print(", ".join([c[2] for c in con.fetchall() if c[4] == sys.argv[4]]))]
+    [print(", ".join([c[2] for c in con.fetchall() if c[4] == argv[4]]))]
     con.close()
     da.close()
