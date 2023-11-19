b478fbb2f7c3257669be90b03e6868111bb2760c
Selomon1 <soya.vandy1950@gmail.com>
2023-11-19 05:09:27 -0800

    Python Object Relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Lists all the states from the given database hbtn_0e_0_usa
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if (__name__ == "__main__"):
+    da = MySQLdb.connect(host= "localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    con = da.cursor()
+    con.execute("SELECT * FROM `states`")
+    for row in con.fetchall():
+        print (row)
+    con.close()
+    da.close()
