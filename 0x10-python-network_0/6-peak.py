c4aaa410d254d4e07f2cf4742d13319633d01e4c
Selomon1 <soya.vandy1950@gmail.com>
2023-11-30 10:51:40 -0800

    Python Network 0

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function that finds a peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+    if not list_of_integers or list_of_integers == []:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid_point = (left + right) // 2
+
+        if list_of_integers[mid_point] < list_of_integers[mid_point + 1]:
+            left = mid_point + 1
+        else:
+            right = mid_point
+
+    return list_of_integers[left]
