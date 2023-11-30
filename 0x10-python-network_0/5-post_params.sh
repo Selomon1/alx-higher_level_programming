2a2451f231cab1a3c9ae448d7ad79e8a75fa6769
Selomon1 <soya.vandy1950@gmail.com>
2023-11-30 10:25:44 -0800

    Python Network 0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a POST request to the passed URL
-curl -s -X POST -d "email=soya.vandy1950@gmail.com&subject=I will always be here for PLD" $1
+curl -X POST -H email=test@gmail.com -d subject="I will always be here for PLD" $1
