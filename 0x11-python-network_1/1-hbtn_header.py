d33b2ba6b899a19d672a96f352972673246f4fe1
Selomon1 <soya.vandy1950@gmail.com>
2023-12-01 06:24:40 -0800

    Python Network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,11 @@ Script that sends a request to the URL and displays the value
 of the "X-Request-Id" found in the header of the response
 """
 
-from urllib.request import urlopen, Request
+from urllib.request import urlopen
 from sys import argv
 
 if __name__ == "__main__":
-    with urlopen(Request(url)) as response:
+    url = argv[1]
+    with urlopen(url) as response:
+        req_id = response.headers['X-Request-Id']
+        print(f"{req_id}")
