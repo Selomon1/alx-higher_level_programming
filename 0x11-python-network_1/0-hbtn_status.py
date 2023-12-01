247f7d93bb46aa69743da27c76612c94925a6105
Selomon1 <soya.vandy1950@gmail.com>
2023-12-01 05:49:15 -0800

    Python Network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ from urllib.request import Request, urlopen
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urlopen(Request(url)) as response:
-        print("Bodey Response:")
+        print("Body response:")
         print("\t- type:", type(response.read()))
         print("\t- content:", response.read())
         print("\t- utf8 content:", response.read().decode('utf8'))
