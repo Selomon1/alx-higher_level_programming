3b7856ed17be556c82cb173e8b07bc3622a827fb
Selomon1 <soya.vandy1950@gmail.com>
2023-12-03 13:16:11 +0300

    Python Network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,14 +10,10 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/user'
+    url = f"https://api.github.com/user"
     username = argv[1]
     password = argv[2]
-    response = requests.get(url, (username, password))
-    if response.status_code == 200:
-        user_id = response.json().get('id')
-        print(user_id)
-    else:
-        print(None)
+    auth = (argv[1], argv[2])
 
-    
+    response = requests.get(url, auth=auth)
+    print(response.json().get('id'))  
