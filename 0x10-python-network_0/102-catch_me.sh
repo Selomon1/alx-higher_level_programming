db9ef9ea59f7af1aa9b06987d4dbeac090431e6f
Selomon1 <soya.vandy1950@gmail.com>
2023-12-04 11:23:35 +0300

    Python Network 0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sending a POST request with a 0.0.0.0:5000/catch_me  that
 # ...tirigger the server respnse
-curl -s -X PUT -L -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
+curl -s -X POST -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me >/dev/null
