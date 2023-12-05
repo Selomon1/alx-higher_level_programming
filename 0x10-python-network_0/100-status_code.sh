8f675bd0afd27069dc9c5a617f3bb3dbf7c6f886
Selomon1 <soya.vandy1950@gmail.com>
2023-12-05 11:34:45 +0300

    Python Network 0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Script that sends a request to a URL passed as an argument, and display
-# ... only the status code of the response.
-# Not allowed to use any pipe, redirection, ; and &&
-
+# Script that sends request and extract only the status code
 curl -s -o /dev/null -w "%{http_code}" $1
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sending a POST request with a 0.0.0.0:5000/catch_me  that
-# ...tirigger the server respnse
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+# Sending a POST request with a 0.0.0.0:5000/catch_me that tirigger the server
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: School"
