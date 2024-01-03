6d582b32f8e40e7e6b5852d79fd811963bb9f65f
Selomon1 <soya.vandy1950@gmail.com>
2024-01-03 14:53:12 +0300

    javascript-web_scraping

---
+++ 0x10-python-network_0/102-catch_me.sh102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sending a POST request with a 0.0.0.0:5000/catch_me  that
+# ...tirigger the server respnse
+curl -s -X PUT -L -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
